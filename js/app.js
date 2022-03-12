8c5673eda38d61438720066598923164581a67cc
Vladimir Kamuz <v.kamuz@gmail.com>
2022-03-12 10:29:31 +0200

    Add book to list

--- js/app.js
+++ js/app.js
@@ -1,1 +1,48 @@
-console.log("Hey");
+// Book Constructor
+function Book(title, author, isbn){
+	this.title = title;
+	this.author = author;
+	this.isbn = isbn;
+}
+
+// UI Constructor
+function UI(){}
+
+// Add book to list
+UI.prototype.addBookToList = function(book){
+	// Get list
+	const list = document.getElementById('book-list');
+	// Create tr element
+	const row = document.createElement('tr');
+	// Insert cols
+	row.innerHTML = `
+	<td>${book.title}</td>
+	<td>${book.author}</td>
+	<td>${book.isbn}</td>
+	<td><a href="#" class="delete">&times;</a></td></td>`;
+	// Insert row to list
+	list.appendChild(row);
+}
+
+// Clear form fields
+UI.prototype.clearFormFields = function(){
+	document.getElementById('title').value = '';
+	document.getElementById('author').value = '';
+	document.getElementById('isbn').value = '';
+}
+
+// Event Listeners
+document.getElementById('book-form').addEventListener('submit', function(e){
+	// Get form values
+	const title = document.getElementById('title').value;
+	const author = document.getElementById('author').value;
+	const isbn = document.getElementById('isbn').value;
+	// Instantiate Book
+	const book = new Book(title, author, isbn);
+	// Instantiate UI
+	const ui = new UI();
+	// Add book to list
+	ui.addBookToList(book);
+	ui.clearFormFields();
+	e.preventDefault();
+})
